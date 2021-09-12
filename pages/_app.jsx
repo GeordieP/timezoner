6e605ff6c1c3d6c0e6b7dc50b1ea61a9ade69d91
Geordie Powers <geordie.powers@gmail.com>
2021-09-12 12:58:00 -0400

    feat: global footer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,13 @@
 import "./main.css";
+import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="root u-flexCol u-fullWidth u-fullHeight u-centerCross u-centerMain">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className="root u-flexCol u-fullWidth u-fullHeight u-centerCross u-centerMain">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
   );
 }
